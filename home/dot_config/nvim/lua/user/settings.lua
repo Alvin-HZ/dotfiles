76ab1ce680585df376ff667751632add4751101d
Alvin <153151121+Alvin-HZ@users.noreply.github.com>
2024-10-14 21:59:01 -0400

    Change to ayamir/nvimdots

---
+++ home/dot_config/nvim/lua/user/settings.lua
@@ -0,0 +1,13 @@
+-- Please check `lua/core/settings.lua` to view the full list of configurable settings
+local settings = {
+	dashboard_image = {
+		"nvim",
+	},
+}
+
+-- Examples
+settings["use_ssh"] = false
+
+settings["colorscheme"] = "catppuccin"
+
+return settings
